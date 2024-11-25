9e46fe3adcd3e3fe47e55bd0e606951bc4f8144f
Noah Bourque <nbourq4@lsu.edu>
2024-11-25 17:30:01 -0600

    push

--- src/test/java/delft/GenericClassTest.java
+++ src/test/java/delft/GenericClassTest.java
@@ -1,7 +1,30 @@
 package delft;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class GenericClassTest {
+public class GenericClassTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @BeforeEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testDoStuff() {
+        GenericClass.doStuff();
+        assertEquals("", outContent.toString());
+    }
 }
